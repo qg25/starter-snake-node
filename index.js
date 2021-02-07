1eff021b4909e4875f1ed3138df082d0a7e3e04f
repl.it user <replituser@example.com>
2021-02-07 08:46:24 +0000

    done avoidance

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.post('/end', handleEnd)
 
 app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
 
+var grideSize
+var grid = []
+var toClear = false
+
 
 function handleIndex(request, response) {
   var battlesnakeInfo = {
@@ -27,6 +31,14 @@ function handleIndex(request, response) {
 
 function handleStart(request, response) {
   var gameData = request.body
+  grideSize = gameData.board.height
+  
+  for (var i=0; i<grideSize; i++) {
+    grid[i] = []
+    for (var j=0; j<grideSize; j++) {
+      grid[i][j] = 'Empty'
+    }
+  }
 
   console.log('START')
   response.status(200).send('ok')
@@ -35,13 +47,148 @@ function handleStart(request, response) {
 function handleMove(request, response) {
   var gameData = request.body
 
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+  var move
+  var health = gameData.you.health
+  var falseUp = false
+  var falseRight = false
+  var falseDown = false
+  var falseLeft = false
+  var midY = Math.floor(gameData.board.height / 2)
+  var midX = Math.floor(gameData.board.width / 2)
 
+  // console.log('Mid: ' + midX + ' , ' + midY)
+
+  // Restrict movement towards wall
+  if(gameData.you.head.y === gameData.board.height - 1) {falseUp = true}
+  if(gameData.you.head.x === gameData.board.width - 1) {falseRight = true}
+  if(gameData.you.head.y === 0) {falseDown = true}
+  if(gameData.you.head.x === 0) {falseLeft = true}
+
+  // Avoid body
+  for (i in gameData.you.body) {
+    if (gameData.you.head.y + 1 === gameData.you.body[i].y && 
+          gameData.you.head.x === gameData.you.body[i].x) {
+      falseUp = true
+    }
+    if (gameData.you.head.x + 1 === gameData.you.body[i].x &&
+          gameData.you.head.y === gameData.you.body[i].y) {
+      falseRight = true
+    }
+    if (gameData.you.head.y - 1 === gameData.you.body[i].y &&
+          gameData.you.head.x === gameData.you.body[i].x) {
+      falseDown = true
+    }
+    if (gameData.you.head.x - 1 === gameData.you.body[i].x &&
+          gameData.you.head.y === gameData.you.body[i].y) {
+      falseLeft = true
+    }
+  }
+
+  console.log('\nfalseUp: ' + falseUp)
+  console.log('falseRight: ' + falseRight)
+  console.log('falseDown: ' + falseDown)
+  console.log('falseLeft: ' + falseLeft + '\n')
+
+  if (health <= grideSize) {
+    console.log("HUNGRY")
+    
+  }
+  else {
+    // Move to corner
+    if(gameData.you.head.x < midX && falseLeft === false) {
+      move = 'left'
+    }
+    else if (gameData.you.head.x > midX && falseRight === false) {
+      move = 'right'
+    }
+    else if (gameData.you.head.y > midY && falseUp === false) {
+      move = 'up'
+    }
+    else if (gameData.you.head.y < midY && falseDown === false) {
+      move = 'down'
+    }
+    else {
+      if(gameData.you.head.y === midY && falseUp === false) {
+        move = 'up'
+      }
+      else if(gameData.you.head.y === midY && falseDown === false) {
+        move = 'down'
+      }
+      else if(gameData.you.head.x === midX && falseLeft === false) {
+        move = 'left'
+      }
+      else if(gameData.you.head.x === midX && falseRight === false) {
+        move = 'right'
+      }
+      else {
+        if (gameData.you.head.x === gameData.you.head.y) {
+          if (gameData.you.head.x !== 0) {
+            if (falseDown === false) {
+              move = 'down'
+            }
+            else if (falseLeft === false) {
+              move = 'left'
+            }
+          }
+          else {
+            if (falseUp === false) {
+              move = 'up'
+            }
+            else if (falseRight === false) {
+              move = 'right'
+            }
+          }
+          console.log('X == Y ' + move)
+        }
+        else {
+          if (gameData.you.head.x < gameData.you.head.y) {
+            if (falseRight === false) {
+              move = 'right'
+            }
+            else if (falseDown === false) {
+              move = 'down'
+            }
+            else if (falseUp === false) {
+              move = 'up'
+            }
+          }
+          else {
+            if (falseLeft === false) {
+              move = 'left'
+            }
+            else if (falseUp === false) {
+              move = 'up'
+            }
+            else if (falseDown === false) {
+              move = 'down'
+            }
+          }
+          console.log('X != Y ' + move)
+        }
+      }
+      // else {
+      //   var possibleMoves = ['up', 'right', 'down', 'left']
+      //   move = possibleMoves[gameData.turn % 4]
+      //   console.log('Mod 4: ' + move)
+      // }
+      console.log('Else: ' + move)
+    }
+  }
   console.log('MOVE: ' + move)
   response.status(200).send({
     move: move
   })
+  console.log('HP: ' + health + ' Size: ' + grideSize)
+  if (health - 1 <= grideSize && toClear !== true) {
+    console.log("Get Food")
+    getFood(gameData)
+    toClear = true
+  }
+  else if (toClear === true) {
+    console.log('call clear')
+    clearGrid()
+    toClear = false
+  }
 }
 
 function handleEnd(request, response) {
@@ -49,4 +196,26 @@ function handleEnd(request, response) {
 
   console.log('END')
   response.status(200).send('ok')
+}
+
+function getFood(gameData) {
+  var x
+  var y
+  for(i in gameData.board.food) {
+    // console.log('\nX: ' + gameData.board.food[i].x + ' Y: ' + gameData.board.food[i].y)
+    x = gameData.board.food[i].x
+    y = gameData.board.food[i].y
+    grid[x][y] = 'Food'
+  }
+  // console.log('\nFood:\n' + grid + '\n\n')
+}
+
+function clearGrid() {
+  for (var i=0; i<grideSize; i++) {
+    grid[i] = []
+    for (var j=0; j<grideSize; j++) {
+      grid[i][j] = 'Empty'
+    }
+  }
+  // console.log('\n\nClear Food:\n' + grid + '\n\n')
 }
